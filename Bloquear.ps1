c31507a51baae05a91bd2543e8531b9a86d33218
DMZ360 <morfo360@gmail.com>
2025-09-27 10:22:29 -0400

    Update Bloquear.ps1

--- Bloquear.ps1
+++ Bloquear.ps1
@@ -9,10 +9,7 @@ function Kill-Spotify {
     Get-Process -Name "SpotifyWebHelper" -ErrorAction SilentlyContinue | Stop-Process -Force
 }
 
-function Write-Text {
-    param([string]$txt)
-    Write-Host $txt
-}
+# La función Write-Text ha sido eliminada.
 
 # --- Función principal ---
 function BlockUpdate {
@@ -30,25 +27,25 @@ function BlockUpdate {
     $modPtrn = "(?<=desktop-update\/.)2(\/update)" # original
 
     if ($old -match $natPtrn) {
-        Write-Text -txt "Spotify: Las actualizaciones ya estan bloqueadas."
+        Write-Host "Spotify: Las actualizaciones ya estan bloqueadas."
         return
     }
     elseif ($old -match $modPtrn) {
         # 1. Crear copia de seguridad solo si no existe
         if (-not (Test-Path $exe_bak)) {
             Copy-Item $spotifyexe $exe_bak
-            Write-Text -txt "Se creó la copia de seguridad: Spotify.bak" -ForegroundColor Green
+            Write-Host "Se creó la copia de seguridad: Spotify.bak" -ForegroundColor Green
         } else {
-            Write-Text -txt "Spotify.bak ya existe. No se sobrescribirá." -ForegroundColor Yellow
+            Write-Host "Spotify.bak ya existe. No se sobrescribirá." -ForegroundColor Yellow
         }
         
         # 2. Aplicar el bloqueo
         $new = $old -replace $modPtrn, '7/update'
         [IO.File]::WriteAllText($spotifyexe, $new, $ANSI)
-        Write-Text -txt "Spotify: ¡Actualizaciones bloqueadas con éxito!" -ForegroundColor Green
+        Write-Host "Spotify: ¡Actualizaciones bloqueadas con éxito!" -ForegroundColor Green
     }
     else {
-        Write-Text -txt "Spotify: Fallo al bloquear las actualizaciones. El patrón del archivo no coincide con versiones conocidas." -ForegroundColor Yellow
+        Write-Host "Spotify: Fallo al bloquear las actualizaciones. El patrón del archivo no coincide con versiones conocidas." -ForegroundColor Yellow
     }
 }
 
@@ -57,3 +54,4 @@ Write-Host ""
 Write-Host "--- Ejecutando el Bloqueador de Actualizaciones ---" -ForegroundColor Cyan
 BlockUpdate
 Write-Host "Proceso finalizado."
+
