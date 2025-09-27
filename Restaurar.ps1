f96ff49752bfd2c31da944105c0f3261f8c34bde
DMZ360 <morfo360@gmail.com>
2025-09-27 02:24:24 -0400

    Update Restaurar.ps1

--- Restaurar.ps1
+++ Restaurar.ps1
@@ -9,7 +9,7 @@ Get-Process -Name "SpotifyWebHelper" -ErrorAction SilentlyContinue | Stop-Proces
 
 # Lógica de restauración
 if (Test-Path $exe_bak) {
-    Write-Host "Iniciando restauración desde Spotify.bak..." -ForegroundColor Yellow;
+    Write-Host "Iniciando restauracion desde Spotify.bak..." -ForegroundColor Yellow;
     
     # 1. Eliminar el archivo .exe modificado
     Remove-Item $spotifyexe -Force -ErrorAction SilentlyContinue;
@@ -21,3 +21,4 @@ if (Test-Path $exe_bak) {
 } else {
     Write-Host " ERROR: No se encontro el archivo de respaldo Spotify.bak en la ruta: $spRoaming. El desbloqueo manual es necesario." -ForegroundColor Red
 }
+
